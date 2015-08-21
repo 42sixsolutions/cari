94033f6775499cbdcfcb33b4cfa5c8cde29e8fe6
Jerry Conner <jconner@42six.com>
2015-08-21 09:09:32 -0400

    Move the chart stuff inside the list callback

--- cari-web/src/main/webapp/scripts/controllers/CariCtrl.js
+++ cari-web/src/main/webapp/scripts/controllers/CariCtrl.js
@@ -3,9 +3,22 @@
 angular.module('cari.controllers').controller('CariCtrl', ["$scope", "$timeout", "CariMapService", "Query",
         function($scope, $timeout, CariMapService, Query) {
 
-    $scope.list = {};
     Query.list().then(function(response) {
-        $scope.list = response.data;
+        $scope.chartOptions = {
+            xmin: response.data.firstDate,
+            xmax: response.data.lastDate,
+            ymin: 0,
+            ymax: 50
+        };
+        $scope.chartData = [];
+
+        var tmpChartData = [];
+        tmpChartData.push({
+            data: [[$scope.chartOptions.xmin, 12], [$scope.chartOptions.xmax, 36]],
+            points: { show: true, radius: 6, lineWidth: 0, fill: true, fillColor: "rgba(255,0,205,0.5)" },
+            lines: { show: false }
+        });
+        $scope.chartData = tmpChartData;
     });
 
     $scope.lists = {
@@ -33,22 +46,6 @@ angular.module('cari.controllers').controller('CariCtrl', ["$scope", "$timeout",
     };
 
     $scope.selected = {};
-
-    $scope.chartOptions = {
-        xmin: $scope.list.firstDate,
-        xmax: $scope.list.lastDate,
-        ymin: 0,
-        ymax: 50
-    };
-    $scope.chartData = [];
-
-    var tmpChartData = [];
-    tmpChartData.push({
-        data: [[$scope.chartOptions.xmin, 12], [$scope.chartOptions.xmax, 36]],
-        points: { show: true, radius: 6, lineWidth: 0, fill: true, fillColor: "rgba(255,0,205,0.5)" },
-        lines: { show: false }
-    });
-    $scope.chartData = tmpChartData;
 
     $scope.updateReport = function(data) {
         $scope.selected.data = data;
